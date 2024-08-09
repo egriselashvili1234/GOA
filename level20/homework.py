45a7bf1b6c3b3f1d664b6485ddf0992d9bf65d3f
egriselashvili1234 <nikoloziegriselashvili@gmail.com>
2024-08-09 18:57:27 +0400

    first commit

---
+++ level20/homework.py
@@ -0,0 +1,38 @@
+#Homework 1
+
+# num = int(input('Input random number: '))
+
+# if num == 18:
+#     print('You won a toy dinosaur!')
+# elif num == 33:
+#     print('You won a toy bunny!')
+# else:
+#     print('You didnt win anything! Try again next time!')
+
+# #input() - ეს ფუნქცია ტერმინალში გამოიტანს ფრჩხილებში შეტანილ წინადადებას. ამ ფუნქციის დახმარებით მომხმარებელს შეუძლია მნიშვნელობა შეიტანოს ტერმინალში, ეს მნიშვნელობა კი მოთავსდება ცვლადში.
+
+#------------------------------------------------------------------------------------#
+
+#Homework 2
+
+# age = input('Enter your age: ')
+
+# if age == 13:
+#     print('You are a kid')
+# elif age > 13 and age < 20:
+#     print('You are a teenager')
+# elif age > 20:
+#     print('You are grown up')
+# else:
+#     print('ERROR! Input valid age')
+
+#-------------------------------------------------------------------------------------#
+
+#Homework 3
+
+# for i in range(20):
+#     if i == 10:
+#         print('Hello world!')
+#     elif i > 10:
+#         print('Bye world!')
+
---
+++ level22/homework.py
@@ -0,0 +1,52 @@
+# List 1
+list1 = [1]  # 1 element
+# Positive indices: 0
+# Negative indices: -1
+
+# List 2
+list2 = [2, 3]  # 2 element
+# Positive indices: 0, 1
+# Negative indices: -2, -1
+
+# List 3
+list3 = [4, 5, 6]  # 3 element
+# Positive indices: 0, 1, 2
+# Negative indices: -3, -2, -1
+
+# List 4
+list4 = [7, 8, 9, 10]  # 4 element
+# Positive indices: 0, 1, 2, 3
+# Negative indices: -4, -3, -2, -1
+
+# List 5
+list5 = [11, 12, 13, 14, 15]  # 5 element
+# Positive indices: 0, 1, 2, 3, 4
+# Negative indices: -5, -4, -3, -2, -1
+
+# List 6
+list6 = [16, 17, 18, 19, 20, 21]  # 6 element
+# Positive indices: 0, 1, 2, 3, 4, 5
+# Negative indices: -6, -5, -4, -3, -2, -1
+
+# List 7
+list7 = [22, 23, 24, 25, 26, 27, 28]  # 7 element
+# Positive indices: 0, 1, 2, 3, 4, 5, 6
+# Negative indices: -7, -6, -5, -4, -3, -2, -1
+
+# List 8
+list8 = [29, 30, 31, 32, 33, 34, 35, 36]  # 8 element
+# Positive indices: 0, 1, 2, 3, 4, 5, 6, 7
+# Negative indices: -8, -7, -6, -5, -4, -3, -2, -1
+
+# List 9
+list9 = [37, 38, 39, 40, 41, 42, 43, 44, 45]  # 9 element
+# Positive indices: 0, 1, 2, 3, 4, 5, 6, 7, 8
+# Negative indices: -9, -8, -7, -6, -5, -4, -3, -2, -1
+
+# List 10
+list10 = [46, 47, 48, 49, 50, 51, 52, 53, 54, 55]  # 10 element
+# Positive indices: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# Negative indices: -10, -9, -8, -7, -6, -5, -4, -3, -2, -1
+
+
+print(list10[2:5:2])
